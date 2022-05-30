5bed12edcbdab5f92072aa3f8773928902a7199c
pradeep <pradeep.misra@gmail.com>
2022-05-30 21:31:37 +0530

    updated styling for navbar links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,12 +4,14 @@ import "../Styles/Navbar.css"
 const Navbar = () => {
   return (
     <div className="navbar">
-      <a href="/home">Home</a>
-      <a href="/about">About</a>
-      <a href="/services">Services</a>
-      <a href="/products">Products</a>
-      <a href="/team">Team</a>
-      <a href="/contact">Contact</a>
+      <div className="link">
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+        <a href="/products">Products</a>
+        <a href="/team">Team</a>
+        <a href="/contact">Contact</a>
+      </div>
     </div>
   )
 }
