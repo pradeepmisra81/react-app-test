0af5a71f29f89d36ad3418cb53276abe8a65fc20
pradeep <pradeep.misra@gmail.com>
2022-05-31 13:29:25 +0530

    added main page in the website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import MainPage from './Components/MainPage';
 import Navbar from './Components/Navbar';
 
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <MainPage />
     </div>
   );
 }
---
+++ src/Components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/MainPage.css"
+
+const MainPage = () => {
+
+  return (
+    <div className="mainPage">
+      <h1>Welcome to the XYZ Technologies !!</h1>
+    </div>
+  )
+}
+
+export default MainPage;
