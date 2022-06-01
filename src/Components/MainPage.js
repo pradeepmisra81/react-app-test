b0b502b2aba9fae693b67cefb856b12b68061d60
pradeep <pradeep.misra@gmail.com>
2022-06-01 19:08:48 +0530

    updated styling for react question/answer 2

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -19,13 +19,63 @@ const MainPage = () => {
           <h2>2. What are the major features of React?</h2>
         </tr>
         <tr>
-          <p className="answer">The major features of React are:{'\n'}</p>
-          <p> <strong>Virtual DOM</strong> - A virtual DOM object is a representation of the original DOM object. It works like a one-way data binding. Whenever any modifications happen in the web application, the entire UI is re-rendered in virtual DOM representation. Then it checks the difference between the previous DOM representation and new DOM. Once it has done, the real DOM will update only the things that have actually changed. This makes the application faster, and there is no wastage of memory.{'\n'}</p>
-          <p> <strong>JSX</strong> - It uses JSX. JSX stands for JavaScript XML. It is a JavaScript syntax extension. Its an XML or HTML like syntax used by ReactJS.{'\n'}</p>
-          <p> <strong>One way data binding</strong> - ReactJS is designed in such a manner that follows unidirectional data flow or one-way data binding. The benefits of one-way data binding give you better control throughout the application.{'\n'}</p>
-          <p> <strong>Components</strong> - ReactJS is all about components. ReactJS application is made up of multiple components, and each component has its own logic and controls. These components can be reusable which help you to maintain the code when working on larger scale projects.{'\n'}</p>
-          <p> <strong>Simplicity</strong> - ReactJS uses JSX file which makes the application simple and to code as well as understand. We know that ReactJS is a component-based approach which makes the code reusable as your need. This makes it simple to use and learn.{'\n'}</p>
-          <p> <strong>Performance</strong> - ReactJS is known to be a great performer. This feature makes it much better than other frameworks out there today. The reason behind this is that it manages a virtual DOM. The DOM is a cross-platform and programming API which deals with HTML, XML or XHTML. The DOM exists entirely in memory. Due to this, when we create a component, we did not write directly to the DOM. Instead, we are writing virtual components that will turn into the DOM leading to smoother and faster performance.{'\n'}</p>
+          <p className="answer">The major features of React are:</p>
+
+          <tr>
+            <td className="answer">
+              <strong>Virtual DOM</strong>
+            </td>
+            <td className="answer">
+              A virtual DOM object is a representation of the original DOM object. It works like a one-way data binding. Whenever any modifications happen in the web application, the entire UI is re-rendered in virtual DOM representation. Then it checks the difference between the previous DOM representation and new DOM. Once it has done, the real DOM will update only the things that have actually changed. This makes the application faster, and there is no wastage of memory.
+            </td>
+          </tr>
+
+          <tr>
+            <td className="answer">
+              <strong>JSX</strong>
+            </td>
+            <td className="answer">
+              It uses JSX. JSX stands for JavaScript XML. It is a JavaScript syntax extension. Its an XML or HTML like syntax used by ReactJS.
+            </td>
+          </tr>
+
+          <tr>
+            <td className="answer">
+              <strong>One way data binding</strong>
+            </td>
+            <td className="answer">
+              ReactJS is designed in such a manner that follows unidirectional data flow or one-way data binding. The benefits of one-way data binding give you better control throughout the application.
+            </td>
+          </tr>
+
+          <tr>
+            <td className="answer">
+              <strong>Components</strong>
+            </td>
+            <td className="answer">
+              ReactJS is all about components. ReactJS application is made up of multiple components, and each component has its own logic and controls. These components can be reusable which help you to maintain the code when working on larger scale projects.
+            </td>
+          </tr>
+
+
+          <tr>
+            <td className="answer">
+              <strong>Simplicity</strong>
+            </td>
+            <td className="answer">
+              ReactJS uses JSX file which makes the application simple and to code as well as understand. We know that ReactJS is a component-based approach which makes the code reusable as your need. This makes it simple to use and learn.
+            </td>
+          </tr>
+
+          <tr>
+            <td className="answer">
+              <strong>Performance</strong>
+            </td>
+            <td className="answer">
+              ReactJS is known to be a great performer. This feature makes it much better than other frameworks out there today. The reason behind this is that it manages a virtual DOM. The DOM is a cross-platform and programming API which deals with HTML, XML or XHTML. The DOM exists entirely in memory. Due to this, when we create a component, we did not write directly to the DOM. Instead, we are writing virtual components that will turn into the DOM leading to smoother and faster performance.
+            </td>
+          </tr>
+
         </tr>
       </table>
 
