9e0360aaa9ec353f7b28f01818faf1b8e7bec051
pradeep <pradeep.misra@gmail.com>
2022-06-05 12:16:31 +0530

    Updated website for new product

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Select from 'react-select';
-import 'react-dropdown/style.css';
+
 
 function App() {
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
-
-
-  const [selectedOption, setSelectedOption] = useState(null);
-  
   return (
     <div className="App">
-      <h1>Select the item in the below dropdown</h1>
-      <Select
-        defaultValue={selectedOption}
-        onChange={setSelectedOption}
-        options={options}
-      />
     </div>
   );
 }
